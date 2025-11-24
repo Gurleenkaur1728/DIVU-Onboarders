3c33a060643836e6209a4152bce5d9cb1bfda293
gurleenkaur1510 <gurleenmahal15@gmail.com>
2025-11-24 14:23:22 -0700

    updated code

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "../src/routes.jsx";
 import { AuthProvider } from "../app/context/AuthContext.jsx";
 import "./index.css";
-import GoogleTranslateWidget from "../src/GoogleTranslateWidget.jsx";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
